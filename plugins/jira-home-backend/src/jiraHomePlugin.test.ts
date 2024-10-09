0a5232f8971caebeb2fae133b84072cedba0923e
anurag pachauri <anuragpachauri8@gmail.com>
2024-10-09 16:31:17 +0530

    Create jiraHomePlugin.test.ts

---
+++ plugins/jira-home-backend/src/jiraHomePlugin.test.ts
@@ -0,0 +1,30 @@
+import { mockServices, startTestBackend } from '@backstage/backend-test-utils';
+import request from 'supertest';
+import jiraHomePlugin from '@internal/backstage-plugin-jira-home-backend'; // Adjust the import to your Jira plugin path
+
+describe('jiraHomePlugin', () => {
+  it('should respond with status 200 on /api/jira-home/project', async () => {
+    // Start the test backend
+    const { server } = await startTestBackend({
+      features: [
+        jiraHomePlugin(),
+        mockServices.rootConfig.factory({
+          data: {
+            jira: {
+              projectKey: '<KEY>',
+              username: 'USERNAME',
+              baseUrl: '<Base Url of the App>',
+              token: 'Basic <Token>',
+            },
+          },
+        }),
+      ],
+    });
+
+    // Make a request to the endpoint
+    const response = await request(server).get('/api/jira-home/project');
+
+    // Check for 200 status response
+    expect(response.status).toBe(200);
+  }, 10000);
+});
